932932e5dcfe4103c650eb2893eb454b84c997eb
lowskydev <code@lowsky.dev>
2025-03-18 19:46:49 +0100

    test error

--- app/user/tests/test_user.py
+++ app/user/tests/test_user.py
@@ -161,3 +161,6 @@ class PrivateUserApiTests(TestCase):
         self.assertEqual(self.user.name, payload['name'])
         self.assertTrue(self.user.check_password(payload['password']))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_test(self):
+        self.assertFalse(True)
