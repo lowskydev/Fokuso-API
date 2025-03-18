57337e224333a4071b02aeceb5d8f343d82cb26a
lowskydev <code@lowsky.dev>
2025-03-18 19:48:28 +0100

    fix test

--- app/user/tests/test_user.py
+++ app/user/tests/test_user.py
@@ -163,4 +163,4 @@ class PrivateUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
     def test_test(self):
-        self.assertFalse(True)
+        self.assertFalse(False)
