4c245367ebdc696224701f940289bf2750e411dd
lowskydev <code@lowsky.dev>
2025-06-26 14:02:42 +0200

    add url paths for weekly and hourly data

--- app/stats/urls.py
+++ app/stats/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CreateFocusSessionView, UserStatsView
+from .views import CreateFocusSessionView, UserStatsView, WeeklyDataView, HourlyDataView
 
 urlpatterns = [
     path('session/', CreateFocusSessionView.as_view(), name='create-session'),
     path('user-stats/', UserStatsView.as_view(), name='user-stats'),
+    path('weekly-data/', WeeklyDataView.as_view(), name='weekly-data'),
+    path('hourly-data/', HourlyDataView.as_view(), name='hourly-data'),
 ]
