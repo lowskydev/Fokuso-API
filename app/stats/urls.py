1aa5102b927bceca002783fc1596c85da5e20611
lowskydev <code@lowsky.dev>
2025-06-26 14:09:14 +0200

    add url for listing sessions

--- app/stats/urls.py
+++ app/stats/urls.py
@@ -4,10 +4,12 @@ from .views import (
     UserStatsView,
     WeeklyDataView,
     HourlyDataView,
+    SessionListView,
 )
 
 urlpatterns = [
     path('session/', CreateFocusSessionView.as_view(), name='create-session'),
+    path('sessions/', SessionListView.as_view(), name='session-list'),
     path('user-stats/', UserStatsView.as_view(), name='user-stats'),
     path('weekly-data/', WeeklyDataView.as_view(), name='weekly-data'),
     path('hourly-data/', HourlyDataView.as_view(), name='hourly-data'),
